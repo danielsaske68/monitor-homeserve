dfed77dfecaed94eeb15a959445a187b1dec489c
juan daniel giraldo alzate <danielsaske68@hotmail.com>
2026-02-20 23:08:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ def home():
 # TELEGRAM
 # ==============================
 def enviar_telegram(mensaje):
+    enviar_telegram("🔔 TEST DE BOT FUNCIONANDO 🔔")
     try:
         url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
         data = {"chat_id": TELEGRAM_CHAT_ID, "text": mensaje}
