ec7bf5f9ba21ed5e621e2bb3f2fa7bf265467a31
juan daniel giraldo alzate <danielsaske68@hotmail.com>
2026-02-23 23:45:23 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-pip install -r requirements.txt
+# build.sh para Render usando Python 3.11 y Playwright
+
+# Activar virtualenv
+source .venv/bin/activate
+
+# Instalar navegadores de Playwright (sin root)
 python -m playwright install
+
+# Iniciar la app con Gunicorn
+gunicorn main:app --bind 0.0.0.0:$PORT
