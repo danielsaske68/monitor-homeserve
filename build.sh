4b7336f5c079654bb41603b5cd37dfed2d809766
juan daniel giraldo alzate <danielsaske68@hotmail.com>
2026-02-24 00:25:00 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
-#!/bin/bash
+# Activar virtualenv
+source /app/.venv/bin/activate
+
+# Instalar los navegadores de Playwright
 python -m playwright install
+
+# Ejecutar gunicorn
+gunicorn main:app --bind 0.0.0.0:$PORT
