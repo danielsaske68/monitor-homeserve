ac8585c95fa5c9d81038dff95010fb7e488f4680
juan daniel giraldo alzate <danielsaske68@hotmail.com>
2026-02-24 00:14:05 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-echo "🔹 Usando Python 3.11 para instalar dependencias..."
-python3.11 -m pip install --upgrade pip
-python3.11 -m pip install -r requirements.txt
-
-echo "🔹 Instalando navegadores para Playwright..."
-python3.11 -m playwright install
-
-echo "✅ Build finalizado."
+python -m playwright install
