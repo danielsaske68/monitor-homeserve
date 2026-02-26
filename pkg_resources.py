74e4f15e2d1c7215699be6701801da30aff0260a
juan daniel giraldo alzate <danielsaske68@hotmail.com>
2026-02-26 10:08:59 +0100

    Update pkg_resources.py

--- pkg_resources.py
+++ pkg_resources.py
@@ -1,7 +1,12 @@
 class DistributionNotFound(Exception):
     pass
 
+
 def get_distribution(name):
     class Fake:
         version = "0.0"
     return Fake()
+
+
+def iter_entry_points(group, name=None):
+    return []
